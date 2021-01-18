7ea3acda9afe9b0a88864232590aedc795ea280b
saoker <saokers@qq.com>
2021-01-18 16:59:36 +0800

    在线翻译

--- demo210118/src/components/HelloWorld.vue
+++ demo210118/src/components/HelloWorld.vue
@@ -1,113 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="Test">
+    <h2>在线翻译</h2>
+    <div class="translate">
+      <Input style="width:35%" v-model="src" :rows="4" type="textarea" placeholder="输入一些文字" />
+      <Button style="height:10%;  margin: 0 4vw; " type="success">自动翻译</Button>
+      <Input style="width:35%" v-model="msg" :rows="4" type="textarea" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "",
+      src: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    translate(val) {
+      this.$axios
+        .get(
+          "http://api.tianapi.com/txapi/fanyi/index?key=6ebe662fe0ca9ab98920152dd3971998&text=" +
+            val
+        )
+        .then((res) => {
+          console.log(res);
+          this.msg = res.data.newslist[0].dst;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  watch: {
+    src(newval, oldval) {
+      this.translate(newval);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.translate {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
 }
 </style>
--- demo210118/src/main.js
+++ demo210118/src/main.js
@@ -3,10 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import Axios from 'axios'
 Vue.config.productionTip = false
-
+// axios需要使用prototype将axios挂载到原型上 ，$后面是自己另起的名称，以后就可以使用该名称
+Vue.prototype.$axios = Axios
 /* eslint-disable no-new */
+import iView from 'iview'; // 导入组件库
+
+import 'iview/dist/styles/iview.css'; // 导入样式
+
+Vue.use(iView);
+
+
 new Vue({
   el: '#app',
   router,
